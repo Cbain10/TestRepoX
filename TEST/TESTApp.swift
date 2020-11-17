3d7c6729ab7ec579d3aba4b45274f247f48f594f
Christian Bain <christianbain@me.com>
2020-11-17 16:48:34 -0700

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by Christian Bain on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
